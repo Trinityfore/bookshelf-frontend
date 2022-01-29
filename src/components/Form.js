ddc3bdb69c0eae82226227d97b192ae8d4a7fd4f
Trinity Fore <trinityfore@icloud.com>
2022-01-29 11:15:52 -0600

    form.js complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import { format } from "node:path/posix";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const Form = (props) => {
@@ -10,7 +10,40 @@ const Form = (props) => {
         setForm({...format,[evt.target.name]:evt.target.value})
     }
 
-    const handleSubmit
+    const handleSubmit = async(evt) => {
+        evt.preventDefault()
+        await fetch(url,{
+            method: props.edit.isEditing ? 'PUT':'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body:JSON.stringify(form)
+        })
+        props.getBook()
+        props.setEdit({
+            isEditing:false,
+            bookToEdit:null,
+        })
+        setForm({title:'', author:'', image:''})
+        history.push('/')
+    }
 
+    useEffect(() => {
+        if(props.edit.isEditing){
+            setForm(props.edit.bookToEdit)
+        }
+    }, [props.edit])
+
+    return(
+        <form onSubmit={handleSubmit}>
+        <input name="title" onChange={handleChange} value={form.title}/>
+        <input name="author" onChange={handleChange} value={form.author}/>
+        <input name="image" onChange={handleChange} value={form.image}/>
+        <button type="submit">Submit</button>
+        </form> 
+
+    )
 
 }
+
+export default Form;
